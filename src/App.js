97cf9e6ae77fc1a601c2b06e7eab58a496f9a08f
Kaja Królikowska <krolikowska.kaja@gmail.com>
2023-02-02 20:37:57 +0100

    Added footer with link to Github repository.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,22 @@ function App() {
       <div className="AppWrap">
         <div className="AppContainer">
           <h1>Dictionary</h1>
-          <button className="btn btn-primary">Test</button>
         </div>
+        <footer>
+          This app was coded by Kaja Królikowska, and it is open-sourced on{" "}
+          <a
+            href="https://github.com/kajakrolikowska/dictionary-app"
+            alt="Github repository"
+            taret="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="/" alt="Netlify link" target="_blank">
+            Netlify
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
