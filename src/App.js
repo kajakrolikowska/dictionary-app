587991a9a2c8259c42bcdda237610f485129d7b2
Kaja Królikowska <krolikowska.kaja@gmail.com>
2023-02-02 13:13:32 +0100

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppWrap">
+        <div className="AppContainer">
+          <h1>Dictionary</h1>
+          <button className="btn btn-primary">Test</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
