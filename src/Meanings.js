1f231b01137920f9300df5f44a8a906d96dfa22c
Kaja Królikowska <krolikowska.kaja@gmail.com>
2023-02-07 22:49:34 +0100

    Added component 'Synonyms'.

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
-  console.log(props.meanings);
-
   return (
     <div className="Meanings">
       <h5>{props.meanings.partOfSpeech}</h5>
-      <p>{props.meanings.definitions[0].definition}</p>
+      {props.meanings.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            {definition.definition}
+            <br />
+            <strong>Example: </strong>
+            <em>{definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,6 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h3>{props.results.word}</h3>
-
         <h5>{props.results.phonetic}</h5>
         <hr />
         {props.results.meanings.map(function (meanings, index) {
