4908a85f9905f77089539688da21af32bb38b2de
Kaja Królikowska <krolikowska.kaja@gmail.com>
2023-02-18 12:52:22 +0100

    Rephrased Photosx.js ccomponent, and css update.

--- src/Photos.js
+++ src/Photos.js
@@ -2,23 +2,22 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-  if (props.photos) {
-    return (
-      <section className="Photos">
-        <div className="row">
-          {props.photos.map(function (photo, index) {
-            return (
-              <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} alt="" className="img-fluid" />
-                </a>
-              </div>
-            );
-          })}
-        </div>
-      </section>
-    );
-  } else {
+  if (props.photos === null) {
     return null;
   }
+  return (
+    <section className="Photos">
+      <div className="row">
+        {props.photos.map(function (photo, index) {
+          return (
+            <div className="col-4" key={index}>
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <img src={photo.src.landscape} alt="" className="img-fluid" />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
